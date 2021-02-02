008f91557712fdfdb02694cd1d6622fff448c4b3
Emmanuel <akhigbemmanuel@gmail.com>
2021-02-02 13:39:56 +0100

    Create header component

---
+++ components/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tech-broo-header">
+      <div class="tech-broo-header-content">
+            <!-- <div></div> -->
+            <div class="place-self-center text-center">
+              <p class="text-xl mb-1" style="color:#00FFF3">BECOME A TECH BRO</p>
+              <h1 class="text-white font-bold mb-8">Land your dream tech Job with ease.</h1>
+            </div>
+            <!-- <div></div> -->
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.tech-broo-header{
+    position: relative;
+    height:90vh;
+    max-height: 744px;
+    background-color: blueviolet;
+}
+.tech-broo-header-content{
+    height: 50%;
+    display: grid;
+    /* grid-template-rows: auto 1fr auto; */
+}
+
+</style>
