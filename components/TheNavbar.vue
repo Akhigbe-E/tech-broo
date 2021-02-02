cdd7027e4e96b8af9513bd5db7046cf1a6664a94
Emmanuel <akhigbemmanuel@gmail.com>
2021-02-02 12:10:51 +0100

    Create navbar component

---
+++ components/TheNavbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="nav-bg block w-full py-5 px-8">
+    <div class="grid grid-cols-3 mx-auto" style="max-width:1100px">
+      <div class="nav-logo">
+        <logo/>
+      </div>
+      <div class="nav-links">
+        <ul class="flex justify-between text-base font-medium items-center text-white">
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Find Jobs</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Contact Us</NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.nav-bg{
+  background-color: #0B0D17;
+}
+
+</style>
