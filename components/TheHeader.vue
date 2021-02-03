b5f8acd5d7c99ddae724325aadc824aaa6cd6c75
Emmanuel <akhigbemmanuel@gmail.com>
2021-02-03 08:04:54 +0100

    Develop header component

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,33 +1,45 @@
 <template>
-  <div class="tech-broo-header">
-      <div class="tech-broo-header-content">
-            <!-- <div></div> -->
-            <div class="place-self-center text-center">
-              <p class="text-xl mb-1" style="color:#00FFF3">BECOME A TECH BRO</p>
-              <h1 class="text-white font-bold mb-8">Land your dream tech Job with ease.</h1>
-            </div>
-            <!-- <div></div> -->
+  <div class="tech-broo-header mb-28">
+    <div class="tech-broo-header-content">
+      <div class="w-full text-center">
+        <p class="text-xl mb-1" style="color: #00fff3">BECOME A TECH BRO</p>
+        <h1 class="text-white font-semibold mb-8 text-4xl">
+          Land your dream tech Job with ease.
+        </h1>
+        <div class="max-w-3xl mx-auto">
+          <search-field />
+          <span>
+            <p class="text-lg text-white text-left mt-4">
+              Popular: Fullstack Developer, Product Designer, UX Designer
+            </p>
+          </span>
+        </div>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style scoped>
-.tech-broo-header{
-    position: relative;
-    height:90vh;
-    max-height: 744px;
-    background-color: blueviolet;
-}
-.tech-broo-header-content{
-    height: 50%;
-    display: grid;
-    /* grid-template-rows: auto 1fr auto; */
+.tech-broo-header {
+  position: relative;
+  height: 90vh;
+  max-height: 744px;
+  background-image: url('~assets/images/header_image.svg'),
+    linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
+  background-blend-mode: overlay;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 
+.tech-broo-header-content {
+  position: absolute;
+  height: 50%;
+  top: 23%;
+  right: 0;
+  left: 0;
+}
 </style>
