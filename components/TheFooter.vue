ac0e8778bc2552d8c40ce97f1d1e2c8644850ad3
Emmanuel <akhigbemmanuel@gmail.com>
2021-02-03 12:20:19 +0100

    Create footer component

---
+++ components/TheFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="py-5" style="background-color: #0b0d17">
+    <div
+      class="flex items-center justify-between container mx-auto"
+      style="max-width: 1100px"
+    >
+      <logo />
+      <p class="text-sm font-normal text-center" style="color: #d9dbe1">
+        © 2021 Techbroo. All rights reserved
+      </p>
+      <div class="flex gap-x-2 text-right sm:gap-x-4">
+        <a class="inline-block" href="#">
+          <img src="~/assets/images/instagram_icon.svg" alt="instagram" />
+        </a>
+        <a class="inline-block" href="#">
+          <img src="~/assets/images/twitter_icon.svg" alt="twitter" />
+        </a>
+        <a class="inline-block" href="#">
+          <img src="~/assets/images/youtube_icon.svg" alt="youtube" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
