a6fcfb4b05a8539f7a66f0ae5bfcc3a4fb732f20
Emmanuel <akhigbemmanuel@gmail.com>
2021-02-03 14:00:06 +0100

    Create base button link

---
+++ components/BaseButtonLink.vue
@@ -0,0 +1,26 @@
+<template>
+  <nuxt-link
+    class="px-12 py-4 text-white rounded-lg outline-none text-base font-medium"
+    style="background-color: #1f9e98"
+    :to="`${to}`"
+  >
+    {{ value }}
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
