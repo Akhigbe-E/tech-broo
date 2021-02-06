c919d23df9503f56805e45cb704004ac7cfabcaf
Emmanuel <akhigbemmanuel@gmail.com>
2021-02-06 20:10:11 +0100

    Develop jobs page

--- pages/jobs.vue
+++ pages/jobs.vue
@@ -1,25 +1,51 @@
 <template>
   <div class="min-h-screen">
-    <div class="header-two w-full h-44"></div>
-    <div class="mx-auto -mt-20 sm:-mt-10" style="max-width: 905px">
+    <div class="header-two w-full h-24 sm:h-44"></div>
+    <div
+      class="mx-auto -mt-6 sm:-mt-10 container px-5"
+      style="max-width: 905px"
+    >
       <search-field />
-      <div class="mt-8 mb-32">
+      <div class="mt-5 sm:mt-8 mb-14 sm:mb-32 flex justify-between sm:block">
         <drop-down
           placeholder="Filter job type"
           :values="['Full time', 'All']"
           @select-option="handleJobTypeSelected"
         />
         <drop-down
-          class="ml-8"
+          class="ml-0 sm:ml-8"
           placeholder="Filter location"
           :values="['Anywhere', 'Remote']"
           @select-option="handleLocationSelected"
         />
       </div>
     </div>
-    <div v-if="!jobs.length" class="my-8 text-center">
+    <div v-if="!jobs.length && $fetchState.pending" class="my-8 text-center">
       <logo class="mx-auto pr-12" />
       <p class="status-text">Loading...</p>
+    </div>
+    <div
+      v-else-if="!jobs.length && !$fetchState.pending"
+      class="my-8 text-center"
+    >
+      <img
+        class="mx-auto"
+        src="~/assets/images/not_found.svg"
+        alt="not found"
+      />
+      <div>
+        <p class="text-xl font-medium mb-3">
+          No Search Results for “{{ searchString }}”
+        </p>
+        <p class="font-normal text-lg mb-4">Try other key words</p>
+        <button
+          class="rounded-lg px-9 py-3"
+          style="color: #1f9e98; background-color: #f4fafa"
+          @click="clearFilter"
+        >
+          Clear Filters
+        </button>
+      </div>
     </div>
     <div v-else-if="$fetchState.error" class="status-text">
       <p>Kindly refresh this page</p>
@@ -80,9 +106,9 @@ export default {
         }${this.location.length ? `&location=${this.location}` : ``}${
           this.searchString ? `&search=${this.searchString}` : ``
         }`
-      )
-        .then((res) => res.json())
-        .then((data) => data.map((job) => new Jobs(job)))
+      ).then((res) => {
+        return res.json().then((data) => data.map((job) => new Jobs(job)))
+      })
     },
     handleJobTypeSelected(e, value) {
       this.fullTime = value === 'Full time'
@@ -99,6 +125,15 @@ export default {
     handlePrevious() {
       if (this.page === 1) return
       this.page -= 1
+      this.fetchJobs()
+    },
+    clearFilter() {
+      let query = Object.assign({}, this.$route.query)
+      delete query.search
+      this.$router.replace({ query })
+      this.searchString = ''
+      this.fullTime = false
+      this.location = ''
       this.fetchJobs()
     },
   },
